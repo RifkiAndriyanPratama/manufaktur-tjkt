753d0b3902dcbb63b40b7b2826674b2ccfc41ef6
xvenz <xvenz@Lenovo.xvenz.com>
2024-12-24 15:23:28 +0700

    edit:sidebar

--- resources/views/admin/management.blade.php
+++ resources/views/admin/management.blade.php
@@ -1,8 +1,3 @@
 <x-layout-admin>
-<h1>management</h1>
-<form method="POST" action="{{ route('logout') }}" style="display: inline;">
-    @csrf
-    <button type="submit" class="btn btn-link">Logout</button>
-</form>
 
 </x-layout-admin>
