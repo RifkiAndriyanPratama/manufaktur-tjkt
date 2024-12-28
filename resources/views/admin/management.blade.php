ca86a1b6f131e996322abf1b75d8c984dc8721da
xvenz <venzzshogun@gmail.com>
2024-12-28 10:09:09 +0700

    add:svg-statistik

--- resources/views/admin/management.blade.php
+++ resources/views/admin/management.blade.php
@@ -156,8 +156,8 @@
                     </select>
                 </div>
                 <div class="flex justify-end">
-                    <button type="button" onclick="document.getElementById('addModal').classList.add('hidden')" class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded mr-2">Batal</button>
-                    <button type="submit" class="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Tambah</button>
+                    <button type="button" onclick="document.getElementById('addModal').classList.add('hidden')" class="flex bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded mr-2">Batal</button>
+                    <button type="submit" class="flex bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Tambah</button>
                 </div>
             </form>
         </div>
