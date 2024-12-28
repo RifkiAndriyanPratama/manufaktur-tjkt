f0d2644ec7b5c1c4adb46587cf1263c3fc4d92a2
xvenz <venzzshogun@gmail.com>
2024-12-28 17:46:25 +0700

    add:toastr

--- resources/views/components/layout-admin.blade.php
+++ resources/views/components/layout-admin.blade.php
@@ -3,7 +3,8 @@
 <head>
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
+   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
+   <link href="{{ asset('toastr/toastr.min.css') }}" rel="stylesheet">
    <title>Dashboard Admin</title>
    @vite(['resources/css/app.css','resources/js/app.js'])
 </head>
@@ -20,5 +21,30 @@
         </div>
       </div>
    </div>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="{{ asset('toastr/toastr.min.js') }}"></script>
+<script>
+    toastr.options = {
+        "closeButton": true,
+        "progressBar": true,
+        "positionClass": "toast-top-right",
+        "timeOut": "2000",
+        "extendedTimeOut": "1000",
+        "showDuration": "300",
+        "hideDuration": "1000"
+    };
+</script>
+
+<script>
+    // Display Toastr Notifications if they exist in the session
+    @if (session()->has('success'))
+        toastr.success("{{ session('success') }}");
+    @elseif (session()->has('error'))
+        toastr.error("{{ session('error') }}");
+    @elseif (session()->has('info'))
+        toastr.info("{{ session('info') }}");
+    @endif
+    </script>
 </body>
 </html>
