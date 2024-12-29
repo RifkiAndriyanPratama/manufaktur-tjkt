30a60d606b7eb25afa37c26c304b74da346f6260
RifkiAndriyanPratama <rifkiandriyanpratama@gmail.com>
2024-12-29 13:31:38 +0700

    add: count data in statistik

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,13 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Barang;
+use App\Models\DetailPeminjaman;
+use App\Models\Kategori;
+use App\Models\Kelas;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
     public function index() {
-        return view('admin.statistik');
-    }
+        $jumlahDetail = DetailPeminjaman::count();
+        $jumlahBarang = Barang::count();
+        $jumlahKategori = Kategori::count();
+        $jumlahKelas = Kelas::count();
 
-    
+        return view('admin.statistik', compact('jumlahDetail', 'jumlahBarang', 'jumlahKategori', 'jumlahKelas'));
+    }
 }
+
--- resources/views/admin/statistik.blade.php
+++ resources/views/admin/statistik.blade.php
@@ -1,3 +1,7 @@
 <x-layout-admin>
-    <h1>d</h1>
+    <h3>Statistik Data</h3>
+    <p>Jumlah Barang yang Tersedia: {{ $jumlahBarang }} barang</p>
+    <p>Jumlah Detail Peminjaman: {{ $jumlahDetail }} detail</p>
+    <p>Jumlah Kategori: {{ $jumlahKategori }} kategori</p>
+    <p>Jumlah Kelas: {{ $jumlahKelas }} kelas</p>
 </x-layout-admin>
