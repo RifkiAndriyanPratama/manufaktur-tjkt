efdc04a826f07889ef34a24f88d645b58b2df40b
xvenz <venzzshogun@gmail.com>
2025-01-03 06:29:34 +0700

    modify: pdf template & web.php

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -4,12 +4,56 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Riwayat Peminjaman</title>
-    @vite(['resources/css/app.css','resources/js/app.js'])
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            font-size: 12px;
+        }
+        table {
+            width: 90%;
+            margin: auto;
+            border-collapse: collapse;
+            background-color: rgba(255, 255, 255, 0.95);
+            border-radius: 8px;
+            overflow: hidden;
+            font-size: 12px;
+        }
+        th, td {
+            padding: 5px;
+            text-align: center;
+            border: 1px solid #ddd;
+        }
+        th {
+            background-color: rgba(85, 145, 235, 0.94);
+            color: white;
+        }
+        td.nama-peminjam {
+            word-break: break-word;
+            white-space: normal;
+        }
+        td.tanggal {
+        text-align: center;
+        vertical-align: top;
+        line-height: 1.2;
+        }
+        .tanggal .tanggal-atas {
+            display: block;
+        }
+        .tanggal .jam-bawah {
+            display: block;
+        }
+        h1 {
+            text-align: center;
+            color: #b91c1c;
+            font-size: 18px;
+            font-weight: bold;
+        }
+    </style>
 </head>
 <body>
-    <h1 class="text-red-700">Riwayat Peminjaman Barang</h1>
-    <table border="1" cellpadding="10" cellspacing="0" class="bg-white rounded-xl">
-        <thead class="bg-gray-300 text-white border rounded-xl">
+    <h1>Riwayat Peminjaman Barang</h1>
+    <table>
+        <thead>
             <tr>
                 <th>No.</th>
                 <th>Nama Peminjam</th>
@@ -19,21 +63,27 @@
                 <th>Kelengkapan Pinjam</th>
                 <th>Kelengkapan Kembali</th>
                 <th>Tanggal Pinjam</th>
-                <th>Tanggal Kembali </th>
+                <th>Tanggal Kembali</th>
             </tr>
         </thead>
         <tbody>
             @foreach($detail as $index => $item)
                 <tr>
                     <td>{{ $index + 1 }}</td>
-                    <td>{{ $item->nama_peminjam }}</td>
+                    <td class="nama-peminjam">{{ $item->nama_peminjam }}</td>
                     <td>{{ $item->peminjaman->kelas->nama_kelas }}</td>
                     <td>{{ $item->barang->nama_barang }}</td>
                     <td>{{ $item->jumlah_pinjam }}</td>
                     <td>{{ $item->kelengkapan_pinjam }}</td>
                     <td>{{ $item->kelengkapan_kembali }}</td>
-                    <th>{{ $item->created_at }}</th>
-                    <th>{{ $item->updated_at }}</th>
+                    <td class="tanggal">
+                        <span class="tanggal-atas">{{ \Carbon\Carbon::parse($item->created_at)->format('d-m-Y') }}</span>
+                        <span class="jam-bawah">{{ \Carbon\Carbon::parse($item->created_at)->format('H:i:s') }}</span>
+                    </td>
+                    <td class="tanggal">
+                        <span class="tanggal-atas">{{ \Carbon\Carbon::parse($item->updated_at)->format('d-m-Y') }}</span>
+                        <span class="jam-bawah">{{ \Carbon\Carbon::parse($item->updated_at)->format('H:i:s') }}</span>
+                    </td>
                 </tr>
             @endforeach
         </tbody>
--- routes/web.php
+++ routes/web.php
@@ -21,36 +21,28 @@ Route::middleware('auth')->group(function () {
 
     // Route kategori
     Route::get('/kategori', [KategoriController::class, 'index' ])->name('kategori.index');
-    Route::get('/kategori/create', [KategoriController::class,'create'])->name('kategori.create');
     Route::post('/kategori', [KategoriController::class,'store'])->name('kategori.store');
-    Route::get('/kategori/{id_kategori}/edit', [KategoriController::class,'edit'])->name('kategori.edit');
     Route::put('/kategori/{id_kategori}', [KategoriController::class, 'update'])->name('kategori.update');
     Route::delete('/kategori/{id_kategori}', [KategoriController::class, 'destroy'])->name('kategori.destroy');
     Route::get('/kategori/search', [KategoriController::class, 'search'])->name('kategori.search');
 
     // Route Barang
     Route::get('/barang', [BarangController::class, 'index' ])->name('barang.index');
-    Route::get('/barang/create', [BarangController::class,'create'])->name('barang.create');
     Route::post('/barang', [BarangController::class,'store'])->name('barang.store');
-    Route::get('/barang/{id_barang}/edit', [BarangController::class,'edit'])->name('barang.edit');
     Route::put('/barang/{id_barang}', [BarangController::class, 'update'])->name('barang.update');
     Route::delete('/barang/{id_barang}', [BarangController::class, 'destroy'])->name('barang.destroy');
     Route::get('/barang/search', [BarangController::class, 'search'])->name('barang.search');
 
     // Route Kelas
     Route::get('/kelas', [KelasController::class, 'index' ])->name('kelas.index');
-    Route::get('/kelas/create', [KelasController::class,'create'])->name('kelas.create');
     Route::post('/kelas', [KelasController::class,'store'])->name('kelas.store');
-    Route::get('/kelas//{id_kelas}/edit', [KelasController::class,'edit'])->name('kelas.edit');
     Route::put('/kelas/{id_kelas}', [KelasController::class, 'update'])->name('kelas.update');
     Route::delete('/kelas/{id_kelas}', [KelasController::class, 'destroy'])->name('kelas.destroy');
     Route::get('/kelas/search', [KelasController::class, 'search'])->name('kelas.search');
 
     // Route Siswa
     Route::get('/siswa', [SiswaController::class, 'index' ])->name('siswa.index');
-    Route::get('/siswa/create', [SiswaController::class,'create'])->name('siswa.create');
     Route::post('/siswa', [SiswaController::class,'store'])->name('siswa.store');
-    Route::get('/siswa//{id_siswa}/edit', [SiswaController::class,'edit'])->name('siswa.edit');
     Route::put('/siswa/{id_siswa}', [SiswaController::class, 'update'])->name('siswa.update');
     Route::delete('/siswa/{id_siswa}', [SiswaController::class, 'destroy'])->name('siswa.destroy');
 
