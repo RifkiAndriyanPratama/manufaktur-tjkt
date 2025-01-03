bf9735fed8374008b673bb07f5d2f04177640e89
xvenz <venzzshogun@gmail.com>
2025-01-04 00:21:26 +0700

    modify: style in xlsx file template

--- app/Exports/RiwayatExport.php
+++ app/Exports/RiwayatExport.php
@@ -3,28 +3,76 @@
 namespace App\Exports;
 
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 use App\Models\DetailPeminjaman;
-class RiwayatExport implements FromCollection
+
+class RiwayatExport implements FromCollection, WithHeadings, ShouldAutoSize, WithStyles
 {
     /**
-    * @return \Illuminate\Support\Collection
-    */
+     * Mengambil Data
+     *
+     * @return \Illuminate\Support\Collection
+     */
     public function collection()
     {
         return DetailPeminjaman::with(['kategori', 'barang', 'peminjaman'])
             ->get()
-            ->map(function ($detail) {
+            ->map(function ($detail, $index) {
                 return [
-                    'Nama Peminjam' => $detail->nama_peminjam,
-                    'Kategori' => $detail->kategori->nama_kategori ?? 'Tidak Ada',
-                    'Barang' => $detail->barang->nama_barang ?? 'Tidak Ada',
-                    'Jumlah Pinjam' => $detail->jumlah_pinjam,
-                    'Kelengkapan Pinjam' => $detail->kelengkapan_pinjam,
-                    'Kelengkapan Kembali' => $detail->kelengkapan_kembali,
-                    'Waktu Peminjaman' => $detail->created_at,
-                    'Waktu Pengembalian' => $detail->updated_at,
+                    $index + 1,
+                    $detail->nama_peminjam,
+                    $detail->barang->nama_barang ?? 'Tidak Ada',
+                    $detail->kategori->nama_kategori ?? 'Tidak Ada',
+                    $detail->jumlah_pinjam,
+                    $detail->kelengkapan_pinjam,
+                    $detail->created_at,
+                    $detail->updated_at,
                 ];
             });
     }
- }
 
+    /**
+     * Header.
+     *
+     * @return array
+     */
+    public function headings(): array
+    {
+        return [
+            'No',
+            'Nama Peminjam',
+            'Barang',
+            'Kategori',
+            'Jumlah Pinjam',
+            'Kelengkapan Pinjam',
+            'Tanggal Peminjaman',
+            'Tanggal Pengembalian',
+        ];
+    }
+
+    /**
+     * styling
+     *
+     * @param Worksheet $sheet
+     * @return array
+     */
+    public function styles(Worksheet $sheet)
+    {
+        // Styling untuk header
+        $sheet->getStyle('A1:H1')->applyFromArray([
+            'font' => ['bold' => true],
+            'alignment' => ['horizontal' => 'center', 'vertical' => 'center'],
+            'fill' => [
+                'fillType' => 'solid',
+                'color' => ['rgb' => 'D3D3D3'],
+            ],
+        ]);
+
+        $sheet->getStyle('A:H')->getAlignment()->setHorizontal('center');
+        
+        return [];
+    }
+}
